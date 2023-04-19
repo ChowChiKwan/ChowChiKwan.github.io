17b4dea054fcdcf5f9329969097334176dbe531a
Jason <ChowChiKwan000@gmail.com>
2023-04-19 14:53:49 +0800

    Create diary_fall-in-love.md.9e74bfb6.lean.js

---
+++ assets/diary_fall-in-love.md.9e74bfb6.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,O as l}from"./chunks/framework.97fbe135.js";const A=JSON.parse('{"title":"Always","description":"","frontmatter":{},"headers":[],"relativePath":"diary/fall-in-love.md"}'),o={name:"diary/fall-in-love.md"},e=l("",4),p=[e];function t(c,r,i,y,D,F){return n(),a("div",null,p)}const _=s(o,[["render",t]]);export{A as __pageData,_ as default};
