88a657157ace235cb38b4affb5c39992c0511107
Jason <ChowChiKwan000@gmail.com>
2023-04-23 17:26:37 +0800

    Create index.js

---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,15 @@
+// .vitepress/theme/index.js
+import DefaultTheme from 'vitepress/theme';
+// import './custom.css';
+import Comment from "./components/Comment.vue";
+// import 'many-pictures/es/style.css'
+// import manyPictures from 'many-pictures';
+  
+export default {
+  ...DefaultTheme,
+  enhanceApp({ app, router, siteData }) {
+    // 注册组件
+    app.component("Comment", Comment);
+    // app.use(manyPictures);
+  },
+};
